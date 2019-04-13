37d4576ec8e2e1cdbb1871a84d50bd13c76a731e
AnErrantRake <stephenanderspeterson@gmail.com>
2019-04-13 13:31:16 -0500

    Updating strings

---
+++ draft/data/strings.js
@@ -0,0 +1,24 @@
+// dictionaries for in-game strings
+var str_mainMenu = {
+  title: "ameloblast",
+  newgame: "new game",
+  learn: "learn",
+}
+
+var str_story = {
+  birth_intro: ["The year is ~~~~. Following a discovery of unknown origin, the world's weather patterns have been replaced by intermittent mealstorms. While instantly solving world hunger, the mealstorms completely destroyed Earth's ecosystem and much of human civilization. What remains is a food-coated wasteland with only small, isolated pockets of human survivors.","The knowledge of oral hygiene disappeared with civilization, leaving the oral health of the few remaining humans vulnerable to the mealstorms. Your mentor succumbed to [disease] before [rgp_pers] could rediscover the lost art of dentistry. In [rgp_poss] moments, [rgp_pers] tasked you and your best friend with finding the [landmark_count] tomes of dental knowledge..."],
+  birth_naming: "Your mentor left you his smartcar 'ameloblast' to ferry you across the wasteland. Preprogrammed with all possible sources of dental knowledge, ameloblast needs only the names of its passengers to begin the journey.",
+  birth_supplies: "ameloblast has only a basic set of supplies, you will need to carefully ration as you proceed.",
+  birth_journey: ["A message wishing you well on your journey",""],
+  death_success: ["A message indicating full completion",""],
+  death_failure: ["A message indicating partial completion",""],
+}
+
+var str_travel = {
+  inst_rest: "Press ENTER to pause",
+  status_date: "Date:",
+  status_environment: "Mealstorms:",
+  status_health: "Oral Health:",
+  status_supplies: "Supplies:",
+  status_next_landmark: "Next landmark:",
+}
