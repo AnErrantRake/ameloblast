6b52bf14e010a7d46916aa41800cdbf715e908e7
AnErrantRake <stephenanderspeterson@gmail.com>
2019-04-14 16:56:02 -0500

    testing

--- draft/view/partial/rest_actions.js
+++ draft/view/partial/rest_actions.js
@@ -3,13 +3,14 @@ function buildRestActionsView(){
   console.log("Building RestActionsView");
 
   const itemSize = 18;
-  // resume travel
 
+  // resume travel
   let resumeTravel = buildMenuOption("resume",itemSize);
+  resumeTravel.position.y = 15;
 
   // wait
-
   let wait = buildMenuOption("wait",itemSize);
+  wait.position.y = 35;
 
   // container
   let cage = new PIXI.Container();
