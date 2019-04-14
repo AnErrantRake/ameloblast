898b512c8d40d013aabde9b248195026cd8fd5e2
AnErrantRake <stephenanderspeterson@gmail.com>
2019-04-14 16:51:30 -0500

    testing

--- draft/view/rest.js
+++ draft/view/rest.js
@@ -4,17 +4,17 @@ function buildRestView(){
   // status
   let status = buildRestStatusView();
   status.position.set(app.screen.width/2, app.screen.height/2);
-  status.pivot.set(status.position.x - (status.width/2),status.position.y - status.height); //centered, anchored to bottom
+//  status.pivot.set(status.position.x - (status.width/2),status.position.y - status.height); //centered, anchored to bottom
 
   // options
   let options = buildRestOptionsView();
   options.position.set(app.screen.width/2, app.screen.height/2);
-  options.pivot.set(options.position.x - options.width,options.position.y); //anchored top right
+//  options.pivot.set(options.position.x - options.width,options.position.y); //anchored top right
 
   // actions
   let actions = buildRestActionsView();
   actions.position.set(app.screen.width/2, app.screen.height/2);
-  actions.pivot.set(actions.position.x,actions.position.y); //anchored top left
+//  actions.pivot.set(actions.position.x,actions.position.y); //anchored top left
 
   // container
   let cage = new PIXI.Container();
