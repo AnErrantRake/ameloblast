0e6b73293ee777f68e36c4f4a1a9d5ac61b40036
AnErrantRake <stephenanderspeterson@gmail.com>
2019-04-14 15:16:02 -0500

    testing

--- draft/view/travel.js
+++ draft/view/travel.js
@@ -15,7 +15,7 @@ function buildTravelView(){
   const ameloblast = new PIXI.AnimatedSprite(textures);
   ameloblast.animationSpeed = (2/60); //2 frames per second
   ameloblast.position.set(app.screen.width, 200);
-  ameloblast.anchor.set(0.0,1.0);
+  ameloblast.anchor.set(0.5);
   ameloblast.play();
 
   // live status
