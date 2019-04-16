095be56a06e22356a7465392fda12bd8ed6af2c3
Stephen Peterson <stephenanderspeterson@gmail.com>
2019-04-16 14:57:04 -0500

    Replaces dummy strings with flavor text

--- draft/data/strings.js
+++ draft/data/strings.js
@@ -6,12 +6,11 @@ var str_mainMenu = {
 }
 
 var str_story = {
-  birth_intro: ["The year is ~~~~. Following a discovery of unknown origin, the world's weather patterns have been replaced by intermittent mealstorms. While instantly solving world hunger, the mealstorms completely destroyed Earth's ecosystem and much of human civilization. What remains is a food-coated wasteland with only small, isolated pockets of human survivors.","The knowledge of oral hygiene disappeared with civilization, leaving the oral health of the few remaining humans vulnerable to the mealstorms. Your mentor succumbed to cancrum oris before he could rediscover the lost art of dentistry. In his final moments, he tasked you and your best friend with finding the 3 tomes of dental knowledge..."],
+  birth_intro: ["The year is ~~~~. Unchecked technological advancement has replaced the world's weather patterns with 'mealstorms', mana from heaven. While instantly solving world hunger, the mealstorms completely destroyed Earth's ecosystem and much of human civilization. What remains is a food-coated wasteland with only small, isolated pockets of human survivors.","The knowledge of oral hygiene disappeared with civilization, leaving the oral health of the few remaining humans vulnerable to the mealstorms. Your mentor succumbed to cancrum oris before he could rediscover the lost art of dentistry. In his final moments, he tasked you and your best friend with finding the 3 tomes of dental knowledge..."],
   birth_naming: "Your mentor left you his smartcar 'ameloblast' to ferry you across the wasteland. Preprogrammed with all possible sources of dental knowledge, you need only provide the names of its passengers to begin your journey.",
-  birth_supplies: "ameloblast has only a basic set of supplies, \nyou will need to carefully ration as you proceed.",
-  birth_journey: ["A message wishing you well on your journey",""],
-  death_success: ["A message indicating full completion",""],
-  death_failure: ["A message indicating partial completion",""],
+  birth_supplies: "ameloblast has only a basic set of supplies,\nyou will need to carefully ration as you proceed.",
+  death_success: ["Long you have journeyed and far you have come. You have avoided the fate of your mentor and fulfilled his last wish. While knowledge is only as useful as its application, you are now equipped with the secrets of oral hygiene. For those who practice this discipline, the mealstorms become the boon they were once intended to be.","Under your tutelage, humanity may rise from the ashes of the old world and build a civilization greater than any that has come before. A civiliation where toothbrushes rain from the sky, floss grows on trees and mouthwash flows like the rivers of old."],
+  death_failure: ["Was it the mealstorms? Were you too frugal? A bad start from which you could never recover? Perhaps it was mere chance. No matter the cause, your fate is sealed.","Perhaps another can find a way to restore what was lost?"],
 }
 
 var str_travel = {
