f9465f3c8668d4461a3c677c693a3216807eaefa
Stephen Peterson <stephenanderspeterson@gmail.com>
2019-04-16 14:38:24 -0500

    Adds flavor text

--- draft/data/information.js
+++ draft/data/information.js
@@ -18,28 +18,12 @@ var LANDMARKS = [
       type: "info",
       graphic: null,
       info: {
-        title: "Testing",
-        description: "A test landmark"
+        title: "Oropharynx",
+        description: "A bearded skeleton of a man stands before you, waving a ream of papers wildly. You can't understand anything he's saying, but the writing on some of the pages looks legible."
       },
       notes: {
         journalEntry: ["Displayed text","page2"],
-        citation: "sources indexed in citations.bib"
-      },
-      position: 300
-    }
-  ),
-
-  new Landmark().buildLandmark(
-    {
-      type: "info",
-      graphic: null,
-      info: {
-        title: "Testing 2",
-        description: "A second test landmark"
-      },
-      notes: {
-        journalEntry: ["Displayed text","page2"],
-        citation: "sources indexed in citations.bib"
+        citation: "BroadbentJonathanM.2011Dpao"
       },
       position: 600
     }
@@ -50,14 +34,30 @@ var LANDMARKS = [
       type: "info",
       graphic: null,
       info: {
-        title: "Testing 3",
-        description: "A third test landmark"
+        title: "Palatoglossal Arch",
+        description: "An elderly woman is lying in the sun, ringed by crinkled papers. She's unresponsive, but some of the papers seem recoverable."
       },
       notes: {
         journalEntry: ["Displayed text","page2"],
-        citation: "sources indexed in citations.bib"
+        citation: "CrocombeLA.2012Isic"
       },
-      position: 900
+      position: 1749
+    }
+  ),
+
+  new Landmark().buildLandmark(
+    {
+      type: "info",
+      graphic: null,
+      info: {
+        title: "Palatine Tonsil",
+        description: "A literal skeleton sits atop an enormous pile of books. Its left hand grasps an ornately detailed and extensive manuscript titled 'The Lost Art of Dentistry'. Unfortunately it disintegrates when you pick it up, leaving only a bookmark covered in hand-written notes."
+      },
+      notes: {
+        journalEntry: ["Displayed text","page2"],
+        citation: "GeethikaB.2016Ohp"
+      },
+      position: 2140
     }
   )
 ]
